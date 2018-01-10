d8ff87dd295366712e91093c0cd444d2348fa2d7
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-10 04:18:29 -0800

    [Studio] Delete screen "app.tabsconfident"

--- client/states.js
+++ client/states.js
@@ -51,14 +51,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.tabsconfident', {
-        views: {
-            app: {
-                controller: 'app_tabsconfident',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.tabsconfident');
-                }
-            }
-        }
     });
 });
