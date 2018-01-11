5fe8271eb3f8e0162b8639b4dbdbc12c602fe96c
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-11 06:41:33 -0800

    [Studio] Modified screen "Details"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -5,16 +5,5 @@ function app_details($scope, app) {
     
     var shipmentDetails = app.data.shipmentDetails.recordset[0];
     
-   // $scope.data.shipmentDetails = app.data.shipmentDetails.recordset[0];
     app.data.shipmentDetails = shipmentDetails;
-    
-    //$scope.data.shipmentDetails = app.shipmentDetails;
-    //console.log($scope.data.shipmentDetails.trackingNumber);
-        
-     /*   
-    setTimeout(function () {
-        $scope.data.shipmentDetails = app.shipmentDetails;
-        console.log($scope.data.shipmentDetails.trackingNumber);
-    }, 1000);
-    */
 }
