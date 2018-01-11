151aeacf436a7e78b78d33d5f819ea913223256a
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-11 05:32:28 -0800

    [Studio] Modified screen "Barcode"

--- client/controllers/app/barcode.js
+++ client/controllers/app/barcode.js
@@ -31,11 +31,8 @@ function app_barcode($scope, app) {
         $scope.app.showLoading('Searching for tracking number: ' + trackingNumber);
         
         // TODO implement DB call with trackingNumber
-        var params = {
-            "trackingNumber": trackingNumber
-        };
         
-        app.call("sql.loadShipmentDetails", params);
+        app.call("sql.loadShipmentDetails", { "trackingNumber": trackingNumber });
        
         var shipmentDetails = {
             trackingNumber: trackingNumber
