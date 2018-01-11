d594cfdf8b2e0798e47b45f72058f055f4f06387
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-11 06:42:31 -0800

    [Studio] Modified screen "Details"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -3,6 +3,25 @@ function app_details($scope, app) {
     'use strict';
     app.init($scope);
     
+    app.data.tabs = [
+    {
+      "name": "General",
+      "icon": "ion-ios-information",
+      "selected": true
+    },
+    {
+      "name": "Shipper",
+      "icon": "ion-ios-bell",
+      "selected": false
+    },
+    {
+      "name": "Recipient",
+      "icon": "ion-help-buoy",
+      "selected": false
+    }
+  ];
+    
+    
     var shipmentDetails = app.data.shipmentDetails.recordset[0];
     
     app.data.shipmentDetails = shipmentDetails;
