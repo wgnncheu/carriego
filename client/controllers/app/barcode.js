09ddb71aeec09339a90f43a0faf5b1a404edc593
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-11 01:43:55 -0800

    [Studio] Modified screen "Barcode"

--- client/controllers/app/barcode.js
+++ client/controllers/app/barcode.js
@@ -39,4 +39,20 @@ function app_barcode($scope, app) {
         app.shipmentDetails = shipmentDetails;
         app.go('app.details');
     };
+    
+    $scope.getShipmentDetails = function (trackingNumber) {
+      
+        const sql = require('mssql')
+ 
+        async () => {
+            try {
+                const pool = await sql.connect('mssql://username:password@localhost/database')
+                const result = await sql.query`select * from mytable where id = ${value}`
+                console.dir(result)
+            } catch (err) {
+                // ... error checks
+            }
+        }  
+    };
+    
 }
