7eba232d3384228618029b81bf7600ade1f0539d
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-11 00:48:38 -0800

    [Studio] Modified screen "Details"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -2,9 +2,9 @@ angular.module('app').controller('app_details', app_details);
 function app_details($scope, app) {
     'use strict';
    
-    
+    app.init($scope);
     $scope.data.trackingNumber = app.detailsData.trackingNumber;
-     app.init($scope);
+  
     alert($scope.data.trackingNumber);
     //$scope.data = extraData;
 }
